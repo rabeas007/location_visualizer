42463fb618e462bbe2c7924e08c6c7c34ec18ee9
amjade <amjad.egabria@amdocs.com>
2020-06-06 20:19:51 +0300

    handle map display and add positon by receiver

--- src/Components/Markers/Marker.js
+++ src/Components/Markers/Marker.js
@@ -1,83 +1,67 @@
-
 import React, {Component} from 'react';
 import './Marker.css';
 import {Avatar,Chip} from '@material-ui/core';
 
 
 export default class extends Component{
-  constructor(props){
-      super(props)
-      this.state={
-        opened: false,
-      }
-      this.timeout = '';
-      this.marker=React.createRef();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.x !== prevProps.x || this.props.y !== prevProps.y) {
-        // let counter = 0;
-        // this.timeout = setTimeout(() => {
-        //     counter ++;
-        //     this.step(counter);
-        //     if(counter > Math.abs(this.props.x - prevProps.x)) {
-        //         clearTimeout(this.timeout)
-        //     }
-        // }, 20)
-        this.handleAnimation(prevProps.x, prevProps.y, this.props.x, this.props.y)
+    constructor(props){
+        super(props);
+        this.state={
+            opened: false,
+        }
     }
-  } 
-  handleAnimation = (prevLeft, prevBottom, left, bottom) => {
-    const keyframesStyle = `
-    @-webkit-keyframes run-${this.props.id} {
-      0%   { left: ${prevLeft}; bottom: ${prevBottom}; }
-      100% { left: ${left}; bottom: ${bottom}; }
-    }`;
-    const styleElement = document.createElement('style');
-    let styleSheet = null;
+    componentDidUpdate(prevProps) {
+        if (this.props.x !== prevProps.x || this.props.y !== prevProps.y) {
 
-    document.head.appendChild(styleElement);
+            this.handleAnimation(prevProps.x, prevProps.y, this.props.x, this.props.y)
 
-    styleSheet = styleElement.sheet;
+        }
+    } 
+    handleAnimation = (prevLeft, prevBottom, left, bottom) => {
+        const keyframesStyle = `
+        @-webkit-keyframes run-${this.props.id} {
+          0%   { left: ${prevLeft}; bottom: ${prevBottom}; }
+          100% { left: ${left}; bottom: ${bottom}; }
+        }`;
+        const styleElement = document.createElement('style');
+        document.head.appendChild(styleElement);
+        const styleSheet = styleElement.sheet;
+        styleSheet.insertRule(keyframesStyle, styleSheet.cssRules.length);
+    };
 
-    styleSheet.insertRule(keyframesStyle, styleSheet.cssRules.length);
-}
-  showLabel=()=>{
-    const { opened } = this.state;
-		this.setState({
-			opened: !opened,
-		});
-  }
-  step = (counter) => {
-    if(this.marker) {
-        this.marker.style.transform = 'translate(' + counter + 'px, ' + counter + 'px)';
+    getPosition = (receiver) => {
+        const roomElement = document.querySelector(`[data-name*="${receiver}"]`);
+        let x = 0, y =0 ;
+        if (roomElement) {
+            const rect = roomElement.getBoundingClientRect();
+            x = Math.random() * (rect.right - rect.left) + rect.left + 10;
+            y = Math.random() * (rect.bottom - rect.top) + rect.top -10;
+        }
+        return {x, y}
+    };
+    render(){
+        const { color, name, id, x, y} = this.props;
+        //this is a fake number to test please change it by receiver number
+          //receiver number should be aligned with the data-name of g element
+        const position = this.getPosition('6095')
+        return (
+            <div style={{left: position.x , top: position.y, position:"absolute", WebkitAnimation: `run-${this.props.id} 1s linear`}} ref={ref => this.marker = ref} id={id} onClick={this.showLabel}>
+                {this.state.opened && <Chip
+                    avatar={<Avatar>M</Avatar>}
+                    variant="outlined"
+                    size="small"
+                    color='primary'
+                    label={id}
+                    onClick={()=>console.log('Hi') }
+                    style={{ transform: 'translate(-55%, -26%)'}}
+                  />}
+                <div
+                  className="pin bounce"
+                  style={{ backgroundColor: color, cursor: 'pointer'}}
+                  title={name + ' '+id}
+                  />
+                <div className="pulse" />
+            </div>)
     }
-
-};
-
-  render(){
-    const { color, name, id, x, y} = this.props;
-    return (
-      <div style={{left:x , bottom:y, position:"absolute", WebkitAnimation: `run-${this.props.id} 1s linear`}} ref={ref => this.marker = ref} id={id} onClick={this.showLabel}>
-        {this.state.opened && <Chip
-            avatar={<Avatar>M</Avatar>}
-            variant="outlined"
-            size="small"
-            color='primary'
-            label={id}
-            onClick={()=>console.log('Hi') }
-            style={{ transform: 'translate(-55%, -26%)'}}
-          />}
-        <div
-          className="pin bounce"
-          style={{ backgroundColor: color, cursor: 'pointer'}}
-          title={name + ' '+id}
-          />
-        <div className="pulse" />
-        {/* <span style={{fontSize:8}}>{id}</span> */}
-
-      </div>
-    )
-  }
 }
 
