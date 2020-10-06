202cf36360b92d459d4b3e1c4b752c64502c4e09
rabea <Shamaly@campus.technoin.ac.il>
2020-10-07 01:54:51 +0300

    Final Version - Beta

---
+++ build/precache-manifest.6d6cf17d187b6579f304094432f6c84a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d534ce31e7f200f59a18134d33163c13",
+    "url": "/location_visualizer/index.html"
+  },
+  {
+    "revision": "d1e585bbcd296ea2e0de",
+    "url": "/location_visualizer/static/css/main.6a13ecd3.chunk.css"
+  },
+  {
+    "revision": "e477cd69eba10e13d548",
+    "url": "/location_visualizer/static/js/2.c53e5a00.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/location_visualizer/static/js/2.c53e5a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e585bbcd296ea2e0de",
+    "url": "/location_visualizer/static/js/main.cf5345b6.chunk.js"
+  },
+  {
+    "revision": "76465482fcf85da186e5",
+    "url": "/location_visualizer/static/js/runtime-main.bf5f00ed.js"
+  },
+  {
+    "revision": "ec343a6927b1f446e7aef69b7560e35f",
+    "url": "/location_visualizer/static/media/6thFloorMapFinal.ec343a69.svg"
+  }
+]);
