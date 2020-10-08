2df92b3d35bc0986db6bf94ebee6ec3866fbd2cf
rabea <Shamaly@campus.technoin.ac.il>
2020-10-08 22:38:58 +0300

    Fix server bug

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const logger= require('morgan')
 const PointApi=require('./api/points')
 
-// if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 // const PORT = 3001;
 const app = express();
 app.set( 'port', ( process.env.PORT || 3001 ));
