8459d37ca5146c00a4719009595832d6be9bf4d8
rabea <Shamaly@campus.technoin.ac.il>
2020-10-10 12:20:48 +0300

    const fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require("path")
 if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 // const PORT = 3001;
 const app = express();
-app.set( 'port', ( process.env.PORT || 5000 ));
+app.set( 'port', ( process.env.PORT || 8000 ));
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('client/build'));
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
 
 app.use(cors());
 
-const router = express.Router();
+// const router = express.Router();
 
 // this is our MongoDB database
 const dbRoute = process.env.MONGODB_URI || 'mongodb+srv://rabeashamaly:rabeashamaly@cluster0-q1h8e.mongodb.net/in-door-locations?retryWrites=true&w=majority';
@@ -42,10 +42,10 @@ db.collection('points').createIndex( { floor_id: 1,  start_date: 1, end_date: 1}
 
 // (optional) only made for logging and
 // bodyParser, parses the request body to be a readable json format
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(logger('dev'));
+// app.use(express.static(path.join(__dirname, "client", "build")))
 
 // this is our get method
 // this method fetches all available data in our database
@@ -66,7 +66,7 @@ app.post('/api/addPoint', PointApi.addPoint);
 // });
 
 // append /api for our http requests
-app.use('/api', router);
+// app.use('/api', router);
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
